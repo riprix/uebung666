6c35db6e12251bf936762c026ba8b0286700d89d
„riprix“ <thomas.saer@googlemail.com>
2014-02-05 15:48:23 +0100

    Erstes Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uebung666::Application.config.secret_key_base = '7889f4c4a4d2adc89ea3ffb9ad8a32f31e8fdb9d7df608a19aee1a4074f369b89ac909ecfb49464cc7fa0d380a3271d8780bd0099a1d75ad3ae7cbce8ce68aab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Uebung666::Application.config.session_store :cookie_store, key: '_uebung666_session'
